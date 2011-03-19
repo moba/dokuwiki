97842d591d2972b492694e68f9078f8baedf1b11
moba <m.bartl@headstrong.de>
2011-03-19 04:54:23 -0400

    added user admin/admin, PD license, default name, use dokuwiki-minimal template

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# user:MD5password:Real Name:email:groups,comma,seperated
+
+
+admin:098f6bcd4621d373cade4e832627b4f6:admin:admin@localhost:admin,user
